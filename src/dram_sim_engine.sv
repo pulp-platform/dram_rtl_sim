ccb54d162e0b4d68e0daa55e45570d61579ec15d
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-03-20 09:53:18 +0100

    engine: Pass clock period as type time

--- src/dram_sim_engine.sv
+++ src/dram_sim_engine.sv
@@ -1,4 +1,4 @@
-// Copyright 2023 ETH Zurich and 
+// Copyright 2023 ETH Zurich and
 // University of Bologna
 
 // Solderpad Hardware License
@@ -9,21 +9,21 @@
 // Author: Chi Zhang <chizhang@iis.ee.ethz.ch>, ETH Zurich
 // Date: 07.June.2023
 
-// clock engine for dram simulation
 
 import "DPI-C" function void run_ns(input int ns);
 
+// clock engine for dram simulation
 module dram_sim_engine #(
-	parameter int unsigned ClkPeriodNs    = 10
-	)(
-	input  logic                 clk_i,      // Clock
-	input  logic                 rst_ni     // Asynchronous reset active low
+	parameter time ClkPeriod	= 1ns
+) (
+	input logic clk_i,	// Clock
+	input logic rst_ni	// Asynchronous reset active low
 );
 
 	always_ff @(posedge clk_i or negedge rst_ni) begin : proc_dram_engine
 		if(rst_ni) begin
 			// run DRAMsys every clk
-			run_ns(ClkPeriodNs);
+			run_ns(int'(ClkPeriod / 1ns));
 		end
 	end
 
