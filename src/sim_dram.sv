fab07478b9662ad21f49ac3b6f0910a31321be06
Tim Fischer <fischeti@iis.ee.ethz.ch>
2024-03-20 14:55:31 +0100

    sim_dram: Improve the display of waves

--- src/sim_dram.sv
+++ src/sim_dram.sv
@@ -21,6 +21,7 @@ import "DPI-C" function void dram_write_strobe(input int dram_id, input int stro
 import "DPI-C" function void dram_send_req(input int dram_id, input longint addr, input longint length , input longint is_write, input longint strob_enable);
 import "DPI-C" function void dram_get_read_rsp(input int dram_id, input longint length, inout byte buffer[]);
 import "DPI-C" function int dram_get_read_rsp_byte(input int dram_id);
+import "DPI-C" function int dram_peek_read_rsp_byte(input int dram_id, input int idx);
 import "DPI-C" function void dram_preload_byte(input int dram_id, input longint dram_addr_ofst, input int byte_int);
 import "DPI-C" function int dram_check_byte(input int dram_id, input longint dram_addr_ofst);
 import "DPI-C" function void dram_load_elf(input string app_path);
@@ -60,19 +61,6 @@ module sim_dram #(
 typedef logic [7:0] my_byte_t;
 
 int dram_id;
-
-int in_flight_read_dram;
-int in_flight_read_this_module;
-
-byte out_buffer[DataWidth/8-1:0];
-
-always_comb begin
-    my_byte_t [DataWidth/8-1:0] tmp;
-    for (int i = 0; i < (DataWidth/8); i++) begin
-        tmp[i] = out_buffer[i];
-    end
-    rdata_o = tmp;
-end
 
 //initialize model (CTRL + DRAM)
 initial begin
@@ -130,84 +118,56 @@ task preload_elf_binary(input string elf_binary );
     dram_load_elf(elf_binary);
 endtask
 
-always_ff @(negedge clk_i or posedge clk_i or negedge rst_ni) begin : proc_dram
+always_ff @(posedge clk_i or negedge rst_ni) begin : proc_dram
     if(~rst_ni) begin
-        in_flight_read_this_module <=0;
-        in_flight_read_dram <= 0;
-        req_ready_o <= 1;
-        rsp_valid_o <= 0;
-        b_valid_o <= 0;
+        req_ready_o <= 1'b0;
+        rsp_valid_o <= 1'b0;
+        b_valid_o <= 1'b0;
+        rdata_o <= '0;
+    end else begin
+        // Default assignments
+        rsp_valid_o <= 1'b0;
+        b_valid_o <= 1'b0;
+        req_ready_o <= 1'b0;
 
-    end else if (clk_i) begin //posedge clk
-
-        if (rsp_valid_o & rsp_ready_i) begin
-            rsp_valid_o <= 0;
-            in_flight_read_this_module = in_flight_read_this_module -1;
-        end
-
-        if (b_valid_o & b_ready_i) begin
-            b_valid_o <= 0;
+        // Request
+        if (req_valid_i & req_ready_o) begin
+            for (int i = 0; i < (DataWidth/8); i++) begin
+                dram_write_buffer(dram_id, wdata_i[8*i +: 8], i);
+                dram_write_strobe(dram_id, wstrb_i[i], i);
+            end
+            dram_send_req(dram_id, longint'(addr_i), (DataWidth/8), longint'(we_i), !(&wstrb_i) && we_i);
         end
 
         if (dram_can_accept_req(dram_id)) begin
-            req_ready_o <= 1;
-        end else begin
-            req_ready_o <= 0;
+            req_ready_o <= 1'b1;
         end
 
-        if (req_valid_i & req_ready_o) begin
-            byte in_buffer[DataWidth/8-1:0];
-            my_byte_t [DataWidth/8-1:0] tmp;
-            automatic int strob_enable = 0;
-            tmp = wdata_i;
+        // Read response
+        if (rsp_valid_o & rsp_ready_i) begin
             for (int i = 0; i < (DataWidth/8); i++) begin
-                automatic int strb_int = wstrb_i[i];
-                if (strb_int == 0) begin
-                    strob_enable = 1;
-                end
-                in_buffer[i] = tmp[i];
-                dram_write_buffer(dram_id, tmp[i], i);
-                dram_write_strobe(dram_id, strb_int, i);
-            end
-            dram_send_req(dram_id, addr_i, DataWidth/8, we_i, strob_enable & we_i);
-            if (~we_i) begin
-                in_flight_read_this_module = in_flight_read_this_module + 1;
-            end
-        end
-        //update inflight read
-        in_flight_read_dram = dram_get_inflight_read(dram_id);
-
-    end else begin//negedge clk
-
-        //response
-        if (~rsp_valid_o) begin
-            if (dram_has_read_rsp(dram_id)) begin
-                int get_byte_int;
-                byte bt;
-                // dram_get_read_rsp(dram_id, DataWidth/8, out_buffer);
-                for (int i = 0; i < (DataWidth/8); i++) begin
-                    get_byte_int = dram_get_read_rsp_byte(dram_id);
-                    bt = get_byte_int;
-                    out_buffer[i] = bt;
-                end
-                rsp_valid_o <= 1;
+                void'(dram_get_read_rsp_byte(dram_id));
             end
         end
 
-        //write rsp
-        if (~b_valid_o) begin
-            if (dram_has_write_rsp(dram_id)) begin
-                int get_write_int;
-                get_write_int = dram_get_write_rsp(dram_id);
-                b_valid_o <= 1;
+        if (dram_has_read_rsp(dram_id)) begin
+            rsp_valid_o <= 1'b1;
+            for (int i = 0; i < (DataWidth/8); i++) begin
+                rdata_o[8*i +: 8] <= dram_peek_read_rsp_byte(dram_id, i);
             end
         end
 
+
+        // Write response
+        if (b_valid_o & b_ready_i) begin
+            void'(dram_get_write_rsp(dram_id));
+        end
+
+        if (dram_has_write_rsp(dram_id)) begin
+            b_valid_o <= 1;
+        end
     end
-
 end
-
-
 
 final begin
     close_dram(dram_id);
